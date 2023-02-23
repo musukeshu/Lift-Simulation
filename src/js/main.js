4ef67f712d27326074e4c746967d378cf1d5ab2f
musukeshu <muskan.jhunjhunwalla@siemens-healthineers.com>
2023-02-23 08:43:45 +0530

    Movement of the lift

--- src/js/main.js
+++ src/js/main.js
@@ -29,11 +29,11 @@ function CreateFloors()
         `<div class='FloorWrapperContainer'>
             <div class="FloorWrapper">
                 <div class="LiftButtons">
-                    <button class="UpBtn">UP</button>
-                    <button class="DownBtn">DOWN</button>
+                    ${i===floors-1?'':`<button class="UpBtn" data-floorNo="${i}">UP</button>`}
+                    ${i===0?'':`<button class="DownBtn" data-floorNo="${i}">DOWN</button>`}
                 </div>
                 <div class="LiftWrapper">
-                    ${i===0?GenerateLifts():""}
+                    ${i===0?GenerateLifts():''}
                 </div>
             </div>
             <div class="Floor">
@@ -57,7 +57,7 @@ function GenerateLifts()
     for(i=0;i<lifts;i++)
     {
         resultantLiftDisplay+=
-        `<div class="Lift">
+        `<div class="Lift" data-pos="${i}" data-status="${"free"}">
             <div class="LGate" id="LGate"></div>
             <div class="RGate" id="RGate"></div>
         </div>`
@@ -66,6 +66,7 @@ function GenerateLifts()
 }
 
 //listening to Submit Button
+//TODO: Add some more checks based on the screen size
 SubmitBtn.addEventListener("click", (e) => {
     e.preventDefault();
     if(Number(NumOfFloors.value)<1)
@@ -81,3 +82,40 @@ SubmitBtn.addEventListener("click", (e) => {
         FinalPage.innerHTML=CreateFloors();
     }
 });
+
+//Listen to the button click
+addEventListener('click', (e) => {
+    if(e.target.classList.contains('UpBtn') || e.target.classList.contains('DownBtn')){
+        moveLift(e.target.dataset.floorno);
+    }
+})
+
+
+//Lift Movement Logic 
+//(TODO: 1) To move the nearest lift  2) Handling the edge case scenario if no lift is free)
+
+function moveLift(targetFloor) {
+
+    const lifts = Array.from(document.getElementsByClassName("Lift"));
+    const availableLift = lifts.find((lift) => lift.dataset.status === "free");
+    const availableLiftOnTargetFloor = lifts.find((lift) => {lift.dataset.status === "free" && Number(lift.dataset.pos) === targetFloor});
+
+    // The lift is already present on the current floor
+    if (availableLiftOnTargetFloor) {
+        availableLiftOnTargetFloor.setAttribute("data-status", "busy");
+        return;
+    }
+
+    //If lift is somewhere else than the targeted floor
+    let distanceToTravel = Math.abs(targetFloor - Number(availableLift.dataset.pos) );
+    availableLift.style.transition = `transform ${distanceToTravel * 2}s linear`;
+    availableLift.style.transform = `translateY(${-200 * targetFloor}px)`;
+    availableLift.setAttribute("data-status", "busy");
+
+    //Make the status of lift free after certain time
+    setTimeout(() => {
+        availableLift.setAttribute("data-status","free");
+      }, distanceToTravel* 2000); // TODO: 1000 will be changed after animating lift doors (as more timeout will be required)
+    availableLift.setAttribute("data-pos", targetFloor);
+
+}
